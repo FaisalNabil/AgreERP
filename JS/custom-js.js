e30e24aa4096bb6130ad9c8cdb95f35104b65b57
sarwarhosen007 <sarwarhosen007@gmail.com>
2017-06-17 21:25:38 +0600

    Modified cropos.html

--- JS/custom-js.js
+++ JS/custom-js.js
@@ -1,45 +1,14 @@
-var app = angular.module("myShoppingList", []); 
-app.controller("myCtrl", function($scope) {
-    $scope.products = [];
-    $scope.addItem = function () {
-        $scope.errortext = "";
-        if (!$scope.addMe) {return;}
-        if ($scope.products.indexOf($scope.addMe) == -1) {
-            $scope.products.push($scope.addMe);
-        } else {
-            $scope.errortext = "The item is already in your shopping list.";
-        }
-    }
-    $scope.removeItem = function (x) {
-        $scope.errortext = "";    
-        $scope.products.splice(x, 1);
-    }
+$(document).ready(function(){
+     $('#aboutCrops').click(function(){
+         event.preventDefault();
+          var week = $("#week option:selected").val();
+          var TextAreaValue = $("textarea#ValueAboutWeek").val();
+
+          if (TextAreaValue.length > 0) {
+             var tableRow = "<tr>"+ "<td>"+week+"</td>"+ "<td>"+TextAreaValue+"</td>"+"</tr>";
+             $("#weekAndTask").append(tableRow); 
+          }
+           
+     });
 });
-
-var appp = angular.module("myShoppingList2", []); 
-appp.controller("myCtrl2", function($scope) {
-    $scope.products = [];
-    $scope.addItem = function () {
-        $scope.errortext = "";
-        if (!$scope.addMe) {return;}
-        if ($scope.products.indexOf($scope.addMe) == -1) {
-            $scope.products.push($scope.addMe);
-        } else {
-            $scope.errortext = "The item is already in your shopping list.";
-        }
-    }
-    $scope.removeItem = function (x) {
-        $scope.errortext = "";    
-        $scope.products.splice(x, 1);
-    }
-});
-angular.bootstrap(document.getElementById("App2"), ['myShoppingList2']);
-
-
-
-/*$('input[type="checkbox"]').on('change', function(e){
-   if(e.target.checked){
-     $('#myModal').modal();
-   }
-});*/
  
