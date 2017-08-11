0cfc77b0e176b3b2bfa1a0b36c5c80e4de715594
sarwarhosen007 <sarwarhosen007@gmail.com>
2017-08-11 14:34:58 +0600

    add header and footer

---
+++ app/view/header.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="CSS/bootstrap.min.css">
+  <link rel="stylesheet" href="CSS/font-awesome.min.css">
+
+  <link rel="stylesheet" href="CSS/w3.css">
+  <link rel="stylesheet" href="CSS/menu.css">
+  <link rel="stylesheet" href="CSS/color.css">
+  <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
