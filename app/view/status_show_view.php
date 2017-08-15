dce6e17dffcce13b2cb1c401685b50b9b900d345
Safaet Hossain <tuhin_42@hotmail.com>
2017-08-15 20:43:04 +0600

    status show file added and cropweeklytask updated

---
+++ app/view/status_show_view.php
@@ -0,0 +1,28 @@
+<?php 
+	if(!isset($isDispatchedByFrontController)){
+		include_once("../view/error.php");
+		die;
+	}
+?>
+<br /><h3>SHOW STATUS</h3><hr/><br />
+<table>
+	<tr>
+		<td><b>Status Id</b></td>
+		<td><b>Done Task</b></td>
+		<td colspan="2"></td>
+	</tr>
+	<?php
+	//print_r($statusList);
+		foreach($statusList as $status){
+			echo"<tr>
+					<td>$status[StatusId]</td>
+					<td>$status[DoneTask]</td>
+					 
+
+					<td><a href='/AgriERP/?status_edit&id=$status[StatusId]'>edit</a></td>
+					<td><a href='/AgriERP/?status_delete&id=$status[StatusId]'>delete</a></td>
+				</tr>";
+		}
+	?>	
+</table>
+<br /><hr />
--- data/cropweeklytask_data_access.php
+++ data/cropweeklytask_data_access.php
@@ -1,13 +1,27 @@
 <?php require_once(APP_ROOT."/lib/data_access_helper.php") ?>
 <?php
 	function addCrop_WeeklytaskToDb($crop_Weeklytask){
-		print_r($crop_Weeklytask);
-		$query = "INSERT INTO CropWeeklytask(WeekNumber, CropId, CropInsectSysId, CropFertSysId, FertilizerTask, InsecticideTask, OtherTask) VALUES('$crop_Weeklytask[WeekNumber]', '$crop_Weeklytask[CropId]', '$crop_Weeklytask[CropInsectSysId]', '$crop_Weeklytask[CropFertSysId]', '$crop_Weeklytask[FertilizerTask]', '$crop_Weeklytask[InsecticideTask]', '$crop_Weeklytask[OtherTask]')";
+		//print_r($crop_Weeklytask);
+		if($crop_Weeklytask[CropInsectSysId] == null || $crop_Weeklytask[InsecticideTask] == null){
+			$query = "INSERT INTO CropWeeklytask(WeekNumber, CropId, CropInsectSysId, CropFertSysId, FertilizerTask, InsecticideTask, OtherTask) VALUES('$crop_Weeklytask[WeekNumber]', '$crop_Weeklytask[CropId]', ' ', '$crop_Weeklytask[CropFertSysId]', '$crop_Weeklytask[FertilizerTask]', ' ', '$crop_Weeklytask[OtherTask]')";
+		}elseif ($crop_Weeklytask[CropFertSysId] == null || $crop_Weeklytask[FertilizerTask] == null) {
+			$query = "INSERT INTO CropWeeklytask(WeekNumber, CropId, CropInsectSysId, CropFertSysId, FertilizerTask, InsecticideTask, OtherTask) VALUES('$crop_Weeklytask[WeekNumber]', '$crop_Weeklytask[CropId]', '$crop_Weeklytask[CropInsectSysId]', ' ', ' ', '$crop_Weeklytask[InsecticideTask]', '$crop_Weeklytask[OtherTask]')";
+		}elseif ($crop_Weeklytask[CropFertSysId] == null && $crop_Weeklytask[FertilizerTask] == null && $crop_Weeklytask[CropInsectSysId] == null && $crop_Weeklytask[InsecticideTask] == null) {
+			$query = "INSERT INTO CropWeeklytask(WeekNumber, CropId, CropInsectSysId, CropFertSysId, FertilizerTask, InsecticideTask, OtherTask) VALUES('$crop_Weeklytask[WeekNumber]', '$crop_Weeklytask[CropId]', ' ', ' ', ' ', ' ', '$crop_Weeklytask[OtherTask]')";
+		}else{
+			$query = "INSERT INTO CropWeeklytask(WeekNumber, CropId, CropInsectSysId, CropFertSysId, FertilizerTask, InsecticideTask, OtherTask) VALUES('$crop_Weeklytask[WeekNumber]', '$crop_Weeklytask[CropId]', '$crop_Weeklytask[CropInsectSysId]', '$crop_Weeklytask[CropFertSysId]', '$crop_Weeklytask[FertilizerTask]', '$crop_Weeklytask[InsecticideTask]', '$crop_Weeklytask[OtherTask]')";
+		}
 		return executeNonQuery($query);
 	}	
 	
 	function editCrop_WeeklytaskToDb($crop_Weeklytask){
-		$query = "UPDATE CropWeeklytask SET WeekNumber='$crop_Weeklytask[WeekNumber]', CropId='$crop_Weeklytask[CropId]', CropInsectSysId='$crop_Weeklytask[CropInsectSysId]', CropFertSysId='$crop_Weeklytask[CropFertSysId]', 'FertilizerTask=$crop_Weeklytask[FertilizerTask]', 'InsecticideTask=$crop_Weeklytask[InsecticideTask]', OtherTask='$crop_Weeklytask[OtherTask]' WHERE WeekId=$crop_Weeklytask[WeekId]";
+		if($crop_Weeklytask[CropInsectSysId] == null || $crop_Weeklytask[InsecticideTask] == null){
+			$query = "UPDATE CropWeeklytask SET WeekNumber='$crop_Weeklytask[WeekNumber]', CropId='$crop_Weeklytask[CropId]',CropFertSysId='$crop_Weeklytask[CropFertSysId]', 'FertilizerTask=$crop_Weeklytask[FertilizerTask]',  WHERE WeekId=$crop_Weeklytask[WeekId]";
+		}elseif ($crop_Weeklytask[CropFertSysId] == null || $crop_Weeklytask[FertilizerTask] == null) {
+			$query = "UPDATE CropWeeklytask SET WeekNumber='$crop_Weeklytask[WeekNumber]', CropId='$crop_Weeklytask[CropId]', CropInsectSysId='$crop_Weeklytask[CropInsectSysId]','InsecticideTask=$crop_Weeklytask[InsecticideTask]', OtherTask='$crop_Weeklytask[OtherTask]' WHERE WeekId=$crop_Weeklytask[WeekId]";
+		}else{
+			$query = "UPDATE CropWeeklytask SET WeekNumber='$crop_Weeklytask[WeekNumber]', CropId='$crop_Weeklytask[CropId]', CropInsectSysId='$crop_Weeklytask[CropInsectSysId]', CropFertSysId='$crop_Weeklytask[CropFertSysId]', 'FertilizerTask=$crop_Weeklytask[FertilizerTask]', 'InsecticideTask=$crop_Weeklytask[InsecticideTask]', OtherTask='$crop_Weeklytask[OtherTask]' WHERE WeekId=$crop_Weeklytask[WeekId]";
+		}		
 		return executeNonQuery($query);
 	}
 	
